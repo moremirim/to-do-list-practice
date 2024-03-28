0e7d953f93e5dd85bfb53bf2c7e81102cc15994d
moremirim <moremirim@gmail.com>
2024-03-28 23:10:53 +0900

    delete some comments

---
+++ ToDoListDraft/OnListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnListViewController.swift
+//  ToDoListDraft
+//
+//  Created by 박미림 on 3/28/24.
+//
+
+import UIKit
+
+class OnListViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
--- ToDoListDraft/ViewController.swift
+++ ToDoListDraft/ViewController.swift
@@ -1,19 +1,99 @@
-//
-//  ViewController.swift
-//  ToDoListDraft
-//
-//  Created by 박미림 on 3/22/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Tasks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+            
+        
+        }
+        updateTasks()
+    }
+    
+    func updateTasks() {
+        
+        tasks.removeAll()
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
+        
+    }
+    @IBAction func didTapAdd() {
+        
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+        }
+    }
+    
+}
+ 
